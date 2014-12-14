ebaa390c6edeb38933d9b107fed2e94f93c9c4cb
Sheldon Hearn <sheldonh@starjuice.net>
2014-12-14 22:40:34 +0200

    Remove stray @Component annotation
    Crept in during dda37f30. Always run the tests, even for things you're sure are just documentation changes.

--- calculation-acceptance-tests/src/main/java/net/starjuice/calculation/acceptance_tests/StepDefinitions.java
+++ calculation-acceptance-tests/src/main/java/net/starjuice/calculation/acceptance_tests/StepDefinitions.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 /**
  *  Glue code used by the {@link DeclarativeAcceptanceTestRunner} to match the steps of the human readable acceptance tests
@@ -13,11 +12,11 @@ import org.springframework.stereotype.Component;
  *  The {@link DeclarativeAcceptanceTestRunner} is responsible for injecting an {@link ImperativeTestProvider} component
  *  scanned from the solution implementer's test suite.
  */
-@Component
 public class StepDefinitions {
 
     ImperativeTestProvider testProvider;
 
+    @SuppressWarnings("SpringJavaAutowiredMembersInspection")
     @Autowired
     public void setTestProvider(ImperativeTestProvider testProvider) {
         this.testProvider = testProvider;
