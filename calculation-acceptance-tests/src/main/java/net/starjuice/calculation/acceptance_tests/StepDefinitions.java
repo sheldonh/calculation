8e995093f23c482165c1e172611027b00e86c37e
Sheldon Hearn <sheldonh@starjuice.net>
2014-12-14 06:51:32 +0200

    Use default location for cucumber.xml

--- calculation-acceptance-tests/src/main/java/net/starjuice/calculation/acceptance_tests/StepDefinitions.java
+++ calculation-acceptance-tests/src/main/java/net/starjuice/calculation/acceptance_tests/StepDefinitions.java
@@ -4,9 +4,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration("classpath*:net/starjuice/calculation/acceptance_tests/cucumber.xml")
 public class StepDefinitions {
 
     ImperativeTestProvider testProvider;
